6e52687e271804381462fd3b7f97595f4feb726c
franklyn-noyola <franklyn.noyola@gmail.com>
2016-04-14 12:09:26 +0200

    Updated

---
+++ AIRBUBS_XLAS_JENKINS/CourseModificationRequest/CourseModificationRequestDeliveryData.java
@@ -0,0 +1,30 @@
+package CourseModificationRequest;
+
+
+
+
+
+import org.junit.Test;
+
+
+
+
+public class CourseModificationRequestDeliveryData extends CourseModificationRequestTemplate {
+	
+@Test		
+	public  void CourseModificationRequest() throws Exception{
+				MainTask = "Course Catalog";
+				CRM = "Course Modification Request";				
+				CourseCatalog.StandardCourseCatalogCreation.CourseStandard();									
+		    	Thread.sleep(3000);
+		    	CourseCatalog.CourseCatalogTemplate.CourseExecution();
+		    	Thread.sleep(3000);		    	
+		    	CourseModificationRequestExecution();
+		    	Thread.sleep(1500);
+		    	System.out.println(RequesTask + " Course Modification Request has been created and sent to Completed, Pending for Synchronization");
+		    	driver.quit();		
+	}	
+
+
+
+}
