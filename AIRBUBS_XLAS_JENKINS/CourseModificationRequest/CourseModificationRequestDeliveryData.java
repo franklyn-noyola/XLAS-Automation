9350a95b708e312cd6a4920294caaf8ac61c000a
franklyn-noyola <franklyn.noyola@gmail.com>
2016-04-14 16:38:27 +0200

    Updated

--- AIRBUBS_XLAS_JENKINS/CourseModificationRequest/CourseModificationRequestDeliveryData.java
+++ AIRBUBS_XLAS_JENKINS/CourseModificationRequest/CourseModificationRequestDeliveryData.java
@@ -14,7 +14,8 @@ public class CourseModificationRequestDeliveryData extends CourseModificationReq
 @Test		
 	public  void CourseModificationRequest() throws Exception{
 				MainTask = "Course Catalog";
-				CRM = "Course Modification Request";				
+				CRM = "Course Modification Request";
+				ValidationData = "DeliveryData";
 				CourseCatalog.DeliveryDataCourseCatalog.DeliveryData();									
 		    	Thread.sleep(3000);		    			    	
 		    	CourseModificationRequestExecution();
