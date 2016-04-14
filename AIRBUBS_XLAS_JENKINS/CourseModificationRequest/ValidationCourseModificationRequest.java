4effaba7c15fefdf4f3936d61621a3938a12be10
franklyn-noyola <franklyn.noyola@gmail.com>
2016-04-14 16:24:20 +0200

    Updated

--- AIRBUBS_XLAS_JENKINS/CourseModificationRequest/ValidationCourseModificationRequest.java
+++ AIRBUBS_XLAS_JENKINS/CourseModificationRequest/ValidationCourseModificationRequest.java
@@ -17,9 +17,7 @@ public class ValidationCourseModificationRequest extends CourseModificationReque
 				ValidationData = "ValidationOnly";
 				CRM = "Course Modification Request";
 				CourseCatalog.StandardCourseCatalogCreation.CourseStandard();	
-				Thread.sleep(3000);
-		    	CourseCatalog.CourseCatalogTemplate.CourseExecution();
-		    	Thread.sleep(3000);
+				Thread.sleep(3000);		    	
 		    	CourseModificationRequestExecution();
 		    	Thread.sleep(1500);
 		    	System.out.println(RequesTask + " Course Modification Request has been created and sent to Completed, Pending for Synchronization");
