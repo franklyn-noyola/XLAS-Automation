2158647e8666c1aad9778d61d621d86af3f01282
franklyn-noyola <franklyn.noyola@gmail.com>
2016-04-21 17:33:21 +0200

    Updated

--- AIRBUBS_XLAS_JENKINS/CourseCatalog/UrgentDeliveryDataCourseCatalog.java
+++ AIRBUBS_XLAS_JENKINS/CourseCatalog/UrgentDeliveryDataCourseCatalog.java
@@ -2,10 +2,6 @@ package CourseCatalog;
 
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
-
-import org.openqa.selenium.*;
-import org.openqa.selenium.support.ui.Select;
 
 
 public class UrgentDeliveryDataCourseCatalog extends CourseCatalogTemplate{
@@ -15,11 +11,11 @@ public class UrgentDeliveryDataCourseCatalog extends CourseCatalogTemplate{
 public void UrgentDeliveryData() throws Exception  {
 			MainTask = "Course Catalog";
 			CourseType = "UrgentAndDelivery";
-
-		    	Thread.sleep(1500);
-		    	CourseExecution();
-				System.out.println(RequesTask + " Urgent and Delivery Data Course Catalog has been created and sent to Pending Delivery Data");
-			    driver.quit();
+			StandardCourseCatalogCreation.CourseStandard();
+		    Thread.sleep(1500);
+		    CourseExecution();
+			System.out.println(RequesTask + " Urgent and Delivery Data Course Catalog has been created and sent to Pending Delivery Data");
+			driver.quit();
 			    			  					  		
 		  }
 	
