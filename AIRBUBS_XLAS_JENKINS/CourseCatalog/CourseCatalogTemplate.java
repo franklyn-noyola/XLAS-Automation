c23861c614ab5d2280660067c987510b485bc039
franklyn-noyola <franklyn.noyola@gmail.com>
2016-04-22 16:44:59 +0200

    Updated

--- AIRBUBS_XLAS_JENKINS/CourseCatalog/CourseCatalogTemplate.java
+++ AIRBUBS_XLAS_JENKINS/CourseCatalog/CourseCatalogTemplate.java
@@ -45,8 +45,7 @@ public class CourseCatalogTemplate {
 		
 
 public static void setUp() throws Exception {
-	System.setProperty("webdriver.ie.driver", driveriePath+"IEDriverServer.exe");
-	driver = new InternetExplorerDriver();
+	System.setProperty("webdriver.ie.driver", driveriePath+"IEDriverServer.exe");	
 	driver.manage().window().maximize();
 	driver.manage().timeouts().implicitlyWait(200, TimeUnit.SECONDS);
 	
